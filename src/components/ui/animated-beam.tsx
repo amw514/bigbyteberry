3d14a216bd70e15b3f7fbd24e723a2c9cba7e6f8
ali munir <alimunirwaris1@gmail.com>
2024-10-02 14:38:09 +0500

    Refactor AnimatedBeam component to use 'let' instead of 'const' for entries in ResizeObserver

--- src/components/ui/animated-beam.tsx
+++ src/components/ui/animated-beam.tsx
@@ -94,8 +94,8 @@ export const AnimatedBeam: React.FC<AnimatedBeamProps> = ({
     // Initialize ResizeObserver
     const resizeObserver = new ResizeObserver((entries) => {
       // For all entries, recalculate the path
-      for (const _ of entries) {
-        // Update path for each entry
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      for (let entry of entries) {
         updatePath();
       }
     });
