0ed92161d81ec39beb177c437fbfb80e3ad56674
ali munir <alimunirwaris1@gmail.com>
2024-10-06 19:41:40 +0500

    Refactor ContactUsForm and CommandCode components for improved styling and functionality; remove unused clipboard copy logic

--- src/components/ContactUsForm.tsx
+++ src/components/ContactUsForm.tsx
@@ -44,7 +44,7 @@ export default function ContactForm() {
   };
   return (
     
-    <div className="max-w-lg w-full mx-auto rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black">
+    <div className="max-w-lg w-full mx-auto rounded-2xl p-4 md:p-8 shadow-input shadow-lg border border-gray-600 bg-white dark:bg-black">
       <h2 className="font-bold text-2xl text-neutral-800 dark:text-neutral-200 mb-2">
         Contact Us
       </h2>
--- src/components/ui/code-component.tsx
+++ src/components/ui/code-component.tsx
@@ -1,7 +1,5 @@
 "use client";
-import { useCopyToClipboard } from "@/hooks/useCopyCode";
 import { ClipboardIcon } from "lucide-react";
-import { toast } from "sonner";
 
 export function CommandCode({ children }: Readonly<{ children: string }>) {
 
@@ -11,15 +9,10 @@ export function CommandCode({ children }: Readonly<{ children: string }>) {
     const body = encodeURIComponent("\n\n"); // This will place the cursor on a new line in the body
     window.location.href = `mailto:${email}?subject=${subject}&body=${body}`;
   };
-  const [copiedText, copy] = useCopyToClipboard();
-  const handleCopyCode = (_code: string) => {
-    copy(children);
-    toast.success(`Command copied: ${copiedText}`);
-  };
   return (
     <button
       className="flex transform-gpu items-center justify-between space-x-3 rounded-md bg-amber-300/20 px-5 py-3 text-amber-800 tracking-tighter transition-all hover:bg-amber-300/15 active:scale-90 active:bg-amber-300/30 dark:text-amber-400"
-      onClick={() => { handleCopyCode(children); handleClick(); }}
+      onClick={handleClick}
       type="button"
     >
       <code className="overflow-ellipsis whitespace-nowrap text-left">
